410ad4d3813161358deb05af101cd42336d028d4
Reagan <reaganekhameye@gmail.com>
2018-04-18 10:04:12 +0100

    Ex 1: Reverse String - Solution 2

--- Ex_1: Reverse_String.js
+++ Ex_1: Reverse_String.js
@@ -6,5 +6,14 @@ function reverseString(str) {
   return str.split('').reverse().join('');
 }
 
+// Solution Two
+function reverseString2(str) {
+  let reverseString = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseString += str[i];
+  }
+  return reverseString;
+}
+
 const result = reverseString('javascript');
 console.log(result);
